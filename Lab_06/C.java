bb79158092dd0a1580309cf197c89fd3bc3c8ba8
Mahdinam-Saif-Prisun <m.saif.prisun@gmail.com>
2025-05-02 21:00:58 +0600

    Lab_06 problem D

--- Lab_06/C.java
+++ Lab_06/C.java
@@ -12,54 +12,32 @@ public class C {
         int init_y = sc.nextInt();
         int destination_x = sc.nextInt();
         int destination_y = sc.nextInt();
-        int source_x = init_x;
-        int source_y = init_y;
+
+        // --- Start Timer After Inputs ---
+        long startTime = System.nanoTime();
+
         int[][][] visited = new int[boardSize][boardSize][3];
         int[][] moveset = new int[][] {{1, 2}, {2, 1}, {-1, -2}, {-2, -1}, {1, -2}, {-2, 1}, {-1, 2}, {2, -1}};
-        int moves = 0;
+        int moves;
 
-        //Arithmetic approximation
-        if(Math.abs(init_x - destination_x) > 8 || Math.abs(init_y - destination_y) > 8) {
-            if(Math.abs(init_x - destination_x) > Math.abs(init_y - destination_y)) {
-                moves = (Math.abs(init_x - destination_x) / 2);
-                source_x = init_x + ((destination_x - init_x) / Math.abs(destination_x - init_x)) * 2 * moves;
-                source_y = init_y + ((destination_y - init_y) / Math.abs(destination_y - init_y)) * (moves % Math.abs(destination_y - init_y));
-            }
-            else {
-                moves = (Math.abs(init_y - destination_y) / 2);
-                source_y = init_y + ((destination_y - init_y) / Math.abs(destination_y - init_y)) * 2 * moves;
-                source_x = init_x + ((destination_x - init_x) / Math.abs(destination_x - init_x)) * (moves % Math.abs(destination_x - init_x));
-            }
-        }
-
-        // After approximation, we use BFS for the smaller scale part
+        //Modified BFS
         Queue<int[]> bfsQ = new LinkedList<>();
-        visited[source_x - 1][source_y - 1][0] = 1;  // Mark the initial position as visited
-        bfsQ.add(new int[] {source_x, source_y, 0});  // Add the start position with step count 0
-
-        // Modified BFS loop to explore the grid
-        while (!bfsQ.isEmpty()) {
+        visited[init_x - 1][init_y - 1] = new int[] {-1, -1, 0};
+        bfsQ.add(new int[] {init_x, init_y, 0});
+        while(!bfsQ.isEmpty()) {
             int[] current = bfsQ.remove();
-            int current_x = current[0];
-            int current_y = current[1];
-            int steps = current[2];
-
-            // If we reached the destination, break
-            if (current_x == destination_x && current_y == destination_y) {
+            if(current[0] == destination_x && current[1] == destination_y) {
                 break;
             }
-
-            // Explore all possible moves
-            for (int[] move : moveset) {
-                int temp_x = current_x + move[0];
-                int temp_y = current_y + move[1];
-
-                // Check if the new position is within bounds
-                if (temp_x > 0 && temp_x <= boardSize && temp_y > 0 && temp_y <= boardSize) {
-                    // If the cell hasn't been visited yet, mark it as visited and add it to the queue
-                    if (visited[temp_x - 1][temp_y - 1][0] == 0) {
-                        visited[temp_x - 1][temp_y - 1] = new int[] {current_x, current_y, steps + 1};  // Record parent and steps
-                        bfsQ.add(new int[] {temp_x, temp_y, steps + 1});  // Add the new position with incremented steps
+            for(int[] movingPos : moveset) {
+                int temp_x = current[0] + movingPos[0];
+                int temp_y = current[1] + movingPos[1];
+                if((temp_x > 0 && temp_x <= boardSize) && (temp_y > 0 && temp_y <= boardSize)) {
+                    if(((Math.abs(destination_x - current[0]) >= Math.abs(destination_x - temp_x) - 3) || (Math.abs(destination_y - current[1]) >= Math.abs(destination_y - temp_y) - 3))) {
+                        if(visited[temp_x - 1][temp_y - 1][0] == 0) {
+                            visited[temp_x - 1][temp_y - 1] = new int[] {current[0], current[1], current[2] + 1};
+                            bfsQ.add(new int[] {temp_x, temp_y, current[2] + 1});
+                        }
                     }
                 }
             }
@@ -70,9 +48,16 @@ public class C {
             moves = -1;
         }
         else {
-            moves += visited[destination_x - 1][destination_y - 1][2];
+            moves = visited[destination_x - 1][destination_y - 1][2];
         }
 
         System.out.println(moves);
+
+
+        long endTime = System.nanoTime();
+
+        // --- Output Runtime in Milliseconds ---
+        double runtimeMs = (endTime - startTime) / 1e6;
+        System.out.printf("Runtime: %.3f ms\n", runtimeMs);
     }
 }
---
+++ Lab_06/D.java
@@ -0,0 +1,66 @@
+package Lab_06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class D {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder("");
+        int[] tempValues = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer :: parseInt).toArray();
+        int numOfNodes = tempValues[0], root = tempValues[1];
+        Node[] tree = new Node[numOfNodes + 1];
+        for(int i = 0; i <= numOfNodes; i++) {
+            tree[i] = new Node();
+        }
+
+        //Creating tree
+        for(int i = 1; i < numOfNodes; i++) {
+            tempValues = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer :: parseInt).toArray();
+            int Node_1 = tempValues[0];
+            int Node_2 = tempValues[1];
+            tree[Node_1].edges.add(Node_2);
+            tree[Node_2].edges.add(Node_1);
+        }
+        tree[root].parent = -1;
+
+        //Finding size
+        int garbage = sizer(tree, root);
+
+        //Queries
+        int queries = Integer.parseInt(br.readLine());
+        for(int i = 0; i < queries; i++) {
+            int node = Integer.parseInt(br.readLine());
+            sb.append(tree[node].subTreeSize);
+            sb.append("\n");
+        }
+        
+        //Output
+        System.out.println("--------------------------------------------------------");
+        System.out.println(sb.toString());
+    }
+
+    //Creating node class
+    public static class Node {
+        int subTreeSize = 1;
+        int parent = 0;
+        ArrayList<Integer> edges = new ArrayList<>();
+    }
+
+    //Size assignment function
+    public static int sizer(Node[] tree, int root) {
+        if(tree[root].edges.isEmpty()) {
+            return 1;
+        }
+        for(int node : tree[root].edges) {
+            if(tree[node].parent == 0) {
+                tree[node].parent = root;
+                tree[root].subTreeSize += sizer(tree, node);
+            }
+        }
+        return tree[root].subTreeSize;
+    }
+}
